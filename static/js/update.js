54b53e2e05ec35d67ac55b04d63bb11646ab7df5
wancat <wancat@pm.me>
2020-02-25 15:16:04 +0800

    加入重設密碼功能
    使用 prompt 取得新密碼，但會以明碼顯示

--- static/js/update.js
+++ static/js/update.js
@@ -30,3 +30,9 @@ function update(button, role, password) {
         alert(err)
     })
 }
+
+function resetPassword(button) {
+    let password = prompt("設定新密碼")
+    update(button, 0, password)
+    alert("密碼已更新")
+}
