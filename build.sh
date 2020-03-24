5e90e44dc6f6f96dcf36c217ec61996050883215
wancat <wancat@pm.me>
2020-03-24 15:52:31 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-ARCH=$1
+OS=$1
+ARCH=$2
 
 echo "go build $ARCH"
-GOARCH=$ARCH go build -o fever-pass
+GOOS=$OS GOARCH=$ARCH go build -o fever-pass
 
 mkdir build
 cd ..
-tar -Jcvf fever-pass/build/fever-pass-$ARCH.tar.xz fever-pass/fever-pass fever-pass/static fever-pass/templates fever-pass/doc
+tar -Jcvf fever-pass/build/fever-pass-$OS-$ARCH.tar.xz fever-pass/fever-pass fever-pass/static fever-pass/templates fever-pass/doc
 cd fever-pass 
 go clean
