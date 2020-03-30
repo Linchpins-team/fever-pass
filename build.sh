c437824b2d3d13a65663bde3576a146cf4c2fc25
wancat <wancat@pm.me>
2020-03-30 22:49:54 +0800

    Update build script
    able to packaging all platform in one command

--- build.sh
+++ build.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
-OS=$1
-ARCH=$2
 
-echo "go build $ARCH"
-GOOS=$OS GOARCH=$ARCH go build -o fever-pass
+DIR="fever-pass"
+DEST="build"
 
-mkdir build
-cd ..
-tar -Jcvf fever-pass/build/fever-pass-$OS-$ARCH.tar.xz fever-pass/fever-pass fever-pass/static fever-pass/templates fever-pass/doc
-cd fever-pass 
-go clean
+clean() {
+    rm -rf $DIR
+}
+
+mk_work_dir() {
+    mkdir -p $DIR
+    cp -r templates $DIR/
+    cp -r static $DIR/
+    cp -r doc $DIR/
+    cp -r testdata $DIR/
+    cp LICENSE $DIR/
+    cp README.md $DIR/
+    mkdir -p $DEST
+}
+
+build() {
+    OS=$1
+    ARCH=$2
+
+    echo "building $OS $ARCH"
+    GOOS=$OS GOARCH=$ARCH go build -o $DIR/fever-pass
+
+    echo "packaging $OS $ARCH"
+    tar -Jcf $DEST/fever-pass_$OS-$ARCH.tar.xz $DIR
+}
+
+clean
+mk_work_dir
+build linux amd64
+build linux 386
+build linux arm
+build windows amd64
+build windows 386
+build darwin amd64
+build darwin 386
+clean
